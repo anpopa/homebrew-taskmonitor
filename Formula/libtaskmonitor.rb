c7af6a24a2ab51fa4339363d83a451a0ebda8d25
Alin Popa <alin.popa@triboo-tech.ro>
2025-10-02 19:42:32 +0300

    Update libtaskmonitor

--- Formula/libtaskmonitor.rb
+++ Formula/libtaskmonitor.rb
@@ -1,8 +1,8 @@
 class Libtaskmonitor < Formula
   desc "TaskMonitor Interface Library"
   homepage "https://gitlab.com/taskmonitor/libtaskmonitor"
-  url "https://gitlab.com/taskmonitor/libtaskmonitor/-/archive/v1.1.2/libtaskmonitor-v1.1.2.tar.gz"
-  sha256 "a3f909ccb772ad7bcfdbb65302133a17a2108fc1df3fa998e3469b650f819dee"
+  url "https://gitlab.com/taskmonitor/libtaskmonitor/-/archive/v1.1.3/libtaskmonitor-v1.1.3.tar.gz"
+  sha256 "e5df22d98d49ed34f6be5ce30ff38b192992ee21dbd0ef05106537d8ec015dc1"
   license "MIT"
 
   depends_on "cmake" => :build
